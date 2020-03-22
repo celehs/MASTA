e5c2d4f33b5b23c7220b5223fc459298af2967c4
Ming Yang <hustyangming@gmail.com>
2020-03-22 07:46:11 +0000

    update

--- R/PETLER-package.R
+++ R/PETLER-package.R
@@ -4,8 +4,9 @@
 #' @title PETLER Package
 #' @description
 #' Implements an algorithm to identify the occurrence of event outcome from trajectories of several predictors.
+#' @details Visit the documentation website for more details. https://celehs.github.io/PETLER/ 
 #' @useDynLib PETLER
-#' @import survival data.table doParallel foreach parallel survC1 rootSolve splines glmnet gglasso rpart rpart.utils  
+#' @import survival doParallel foreach data.table survC1 rootSolve splines glmnet gglasso rpart rpart.utils  
 #' @importFrom Rcpp sourceCpp
 #' @importFrom grDevices dev.off pdf
 #' @importFrom graphics hist lines plot
@@ -26,16 +27,25 @@ NULL
 #' @param n_core an integer to specify the number of core using for parallel computing. Default is \code{4}.
 #' @export
 petler.base <- function(data, PPIC_K = FALSE, n.grid = 401, propvar = 0.85, n_core = 4) {
-  
   TrainSurv <- data.frame(data$TrainSurv)
   ValidSurv <- data.frame(data$ValidSurv)
   TrainCode <- data.frame(data$TrainCode)
   ValidCode <- data.frame(data$ValidCode)
-  TrainN <- data.frame(data$TrainN)
-  ValidN <- data.frame(data$ValidN)
   TrainSurv_pred_org <- TrainSurv[-(1:4)]
-  ValidSurv_pred_org <- TrainSurv[-(1:4)]
-  codes <- names(TrainCode)[grep("pred", names(TrainCode))]
+  ValidSurv_pred_org <- ValidSurv[-(1:4)]
+  colnames(TrainSurv) <- colnames(ValidSurv) <- c(
+    "case", "delta", "sx", "sc", paste0("base_pred", 1:(NCOL(TrainSurv) - 4)))
+  colnames(TrainCode)[1:2] <- colnames(ValidCode)[1:2] <- c("case", "analysisfu")  
+  TrainN <- rowsum(TrainCode[, -(1:3)], group = TrainCode[, 1])
+  ValidN <- rowsum(ValidCode[, -(1:3)], group = ValidCode[, 1])
+  TrainN <- cbind(rownames(TrainN), TrainN)
+  ValidN <- cbind(rownames(ValidN), ValidN)  
+  colnames(TrainN)[1] <- colnames(TrainCode)[1]
+  colnames(ValidN)[1] <- colnames(ValidCode)[1]
+  colnames(TrainN)[-1] <- paste0(colnames(TrainCode)[-(1:3)], "_total")
+  colnames(ValidN)[-1] <- paste0(colnames(ValidCode)[-(1:3)], "_total")
+  # codes <- names(TrainCode)[grep("pred", names(TrainCode))]
+  codes <- names(TrainCode[, -(1:3)])
   nn <- nrow(TrainSurv) # labeled
   NN <- length(unique(TrainCode$case)) - nn # unlabeled
   nnv <- nrow(ValidSurv) # validation
@@ -75,6 +85,7 @@ petler.base <- function(data, PPIC_K = FALSE, n.grid = 401, propvar = 0.85, n_co
     t1   = t1[!is.na(t1)]
     
     ### (ii)
+    registerDoParallel(cores=n_core)
     if(PPIC_K){
       tmp = PP_FPCA_CPP_Parallel(t, h1 = bw.nrd(t), h2 = bw.nrd(t)^{5/6}, TrainNP,
                                  bw = "nrd", ngrid = n.grid, Tend = Tend,
@@ -279,10 +290,17 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
       ValidFt[tmp,5*(i-1)+3] = ValidFt[tmp,5*(i-1)+1]
     }
   }
+  ###########################################################
+  #####  log scale
   aa     = c(1:{5*length(codes)})[c(1:{5*length(codes)})%%5%in%c(1,2,3)]
   TrainFt[,aa] = log(TrainFt[,aa])
   ValidFt[,aa] = log(ValidFt[,aa])
   FirstCode = exp(ValidFt[,seq(1,{5*length(codes)},5)])
+  # write.table(FirstCode,paste0(datadir,"CleanData_new_std/FirstCode.dat"),
+  #             row.names = FALSE)
+  ###############################################################
+  ###   Regress out baseline
+  ###############################################################
   txt = paste0("lm(x~", paste(paste0("base_pred", 1:length(TrainSurv_pred_org)), collapse="+"),",data=TrainSurv)")
   func_1 = function(x){
     tmp = eval(parse(text = txt))
@@ -292,11 +310,15 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
   TrainFt = lapply(TrainFt,func_1)
   RegCoef = do.call(cbind,lapply(TrainFt,`[[`,2))
   TrainFt = do.call(cbind,lapply(TrainFt,`[[`,1))
+  #--
   txt = paste0("ValidFt[,l]-as.matrix(cbind(1,ValidSurv[,c('", paste(paste0("base_pred",1:length(ValidSurv_pred_org)),collapse="','"), "')]))%*%RegCoef[,l]")
   ValidFt = sapply(1:ncol(ValidFt),function(l){
     eval(parse(text = txt))
   })
   colnames(ValidFt) = colnames(TrainFt)
+  ###################################################################################
+  ### get estimating equation for beta!!!!!!!!!!!!!!
+  ################################ survival data
   SX     = TrainSurv$sx
   SC     = TrainSurv$sc
   Delta  = TrainSurv$delta
@@ -314,21 +336,38 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
   G_SX[sort(SX,index.return=TRUE)$ix] = G_SX
   G_SX[G_SX==0] = min(G_SX[G_SX>0])
   G_tseq[G_tseq==0] = min(G_tseq[G_tseq>0])
+  
+  #### get initial values using Cheng et al (1995,1997)
+  
+  ####### number of patients with no codes in the labeled set (training)+SEER
+  # corrplot(cor(TrainN[,-1]),title="Total number of code times",
+  #          mar=c(0,0,1,0),addCoef.col = "black",diag=FALSE,type="upper")  # Mostly postively correlated, high correlation
   TrainZC = sapply(seq_along(codes),function(i) sum(TrainN[,i+1]==0))
   names(TrainZC) = codes
-  TrainZC/nrow(TrainN)
+  # TrainZC/nrow(TrainN)
+  
+  ####### number of patients with no codes in the labeled set (validation)
+  # corrplot(cor(ValidN[,-1]),title="Total number of code times",
+  #          mar=c(0,0,1,0),addCoef.col = "black",diag=FALSE,type="upper")  # Mostly postively correlated, high correlation
   ValidZC     = sapply(seq_along(codes),function(i) sum(ValidN[,i+1]==0))
   names(ValidZC) = codes
-  ValidZC/nrow(ValidN)
+  # ValidZC/nrow(ValidN)
+  
+  # for codes with > 70% patients no codes, only use first code time
   codes0  = which({TrainZC+ValidZC}/{nrow(TrainN)+nrow(ValidN)} > thresh)
   codes0  = c(sapply(codes0, function(x) (x-1)*5+2:4))
   codesK  = lapply(1:length(codes),function(x){
     tmp = (x-1)*5+1:5
     tmp = tmp[!tmp%in%codes0]
   })
+  
   Z = as.matrix(cbind(TrainSurv[,paste0("base_pred", 1:length(TrainSurv_pred_org))],TrainFt))
+  
+  ### standardize Z
   meanZ    = apply(Z,2,mean)
   sdZ      = apply(Z,2,sd)
+  
+  
   for(m in 1:length(TrainSurv_pred_org)){
     aa = unique(TrainSurv[,paste0("base_pred",m)])
     if(length(aa)==2){
@@ -337,7 +376,9 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
       sdZ[paste0("base_pred",m)]   = 1
     }
   }
+  
   Z = {Z-VTM(meanZ,nrow(Z))}/VTM(sdZ,nrow(Z))
+  
   # PCA within each code to pre-select features
   TrainFt_PCA = lapply(1:length(codes),function(i){
     if(length(codesK[[i]])>0){
@@ -356,30 +397,49 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
   TrainFt_PCA = do.call(cbind,lapply(TrainFt_PCA,`[[`,2))
   colnames(TrainFt_PCA) = PCAnames
   Z        = cbind(Z[,(1:length(TrainSurv_pred_org))],TrainFt_PCA)
+  
   #--group: a vector of consecutive integers describing the grouping of the coefficients
   group = do.call(c,sapply(1:length(PCA_K),function(i) rep(i,PCA_K[i])))
+  
   #--add grouping for covariaates and update the group vevtor--
   if(is.null(cov_group)){
     cov_group = seq(1:length(TrainSurv_pred_org))
   }
   group = c(cov_group, group+cov_group[length(cov_group)])
+  
   corZExtreme(Z,0.7)
+  # write.table(Z,paste0(datadir,"CleanData_new_std/TrainFt_PCA_Orth_Base.dat"))
+  ### try delete ChP, feel like Pk is estimated better
+  
+  #### B-spline setting
   degree = 1
   knots  = quantile(SX[Delta==1],prob=seq(0.1,0.9,0.1))
   Boundary.knots = c(0,max(SX))
   allknots = c(Boundary.knots[1],knots,Boundary.knots[2])
   q      = length(knots)+degree+1
+  
+  ########################
+  #### Initial values
+  ########################
+  ####--Option I: Cheng et al (1995,1997) Not as good as NPMLE initial
   set.seed(seed)
+  
   func1 = function(bb, Z, Delta, G_SX, SX){estbb.data(bb=bb, Z=Z, Delta=Delta, G_SX=G_SX, SX=SX)$est}
   func2 = function(bb, Z, Delta, G_SX, SX){estbb.data(bb=bb, Z=Z, Delta=Delta, G_SX=G_SX, SX=SX)$Jacob}
+  
   bb    = multiroot(f=func1, start=runif(ncol(Z),-0.1,0.1), jacfunc=func2, Z=Z, Delta=Delta, G_SX=G_SX, SX=SX)
   bb_Cheng = bb.init = bb$root
+  # write.table(bb_Cheng,paste0(datadir,"ParEst_new_std_orth_base/bb_Cheng.dat"),row.names = FALSE)
   ht    = sapply(seq_along(tseq),function(i){
     est = function(a) mean({SX>=tseq[i]}/G_tseq[i]+g_fun(a+Z%*%bb_Cheng))-1
     if(est(-1e10)>0) -1e10 else uniroot(est,lower=-1e10,upper=1e10)$root
   })
   ht    = sort(ht)
   bbt_Cheng = cbind(tseq,ht)
+  # write.table(bbt_Cheng,paste0(datadir,"ParEst_new_std_orth_base/bbt_Cheng.dat"),row.names = FALSE)
+  
+  ### add more weights to origin to get better estimation there
+  ### on original scale
   ht[1] = -30
   weit  = (2/{tseq[-1]+tseq[-length(tseq)]})^{1/4}
   bgi   = function(bg){
@@ -394,11 +454,29 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
                         fn = function(bg) bgi(bg)$fn,
                         gr= function(bg) bgi(bg)$gr,method = "BFGS",
                         control = list(maxit = 30000))
+  
+  # pdf(file=paste0(outdir, "bginit_FromCheng-plot1.pdf"))
+  # plot(tseq[-1],exp(ht[-1]),type="l")
+  # lines(tseq,h.fun(tseq,knots,Boundary.knots,bg.init.Cheng$par),col="red")
+  # dev.off()
+  
+  # pdf(file=paste0(outdir, "bginit_FromCheng-plot2.pdf"))
+  # plot(tseq[-1],ht[-1],type="l")
+  # lines(tseq,log(h.fun(tseq,knots,Boundary.knots,bg.init.Cheng$par)),col="red")
+  # write.table(bg.init.Cheng$par,paste0(datadir,"ParEst_new_std_orth_base/bginit_FromCheng.dat"),row.names = FALSE)
+  # dev.off()
+  
   bg.init.Cheng = bg.init.Cheng$par
+  
+  ####--Option II: NPMLE (use Cheng as initial)
   bbt.init = log(diff(c(0,exp(ht))))
+  # bbt.init = log(diff(c(0,h.fun(tseq,knots,Boundary.knots,bg.init.Cheng))))
   tmp = NPMLE.est(bb.init,bbt.init,SX,Z,Delta) ## either use the bb.init from Chen1995 or an arbitrary initial
   bb_NPMLE  = tmp$bb
   bbt_NPMLE = tmp$bbt
+  # write.table(bb_NPMLE,paste0(datadir,"ParEst_new_std_orth_base/bb_NPMLE.dat"),row.names = FALSE)
+  # write.table(bbt_NPMLE,paste0(datadir,"ParEst_new_std_orth_base/bbt_NPMLE.dat"),row.names = FALSE)
+  
   ht    = bbt_NPMLE[,2]
   weit  = (2/{tseq[-1]+tseq[-length(tseq)]})^{1/4}
   bgi   = function(bg){
@@ -414,20 +492,51 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
                         gr= function(bg) bgi(bg)$gr,method = "BFGS",
                         control = list(maxit = 30000))
   bg.init.NPMLE = bg.init.NPMLE$par
+  
+  # pdf(file=paste0(outdir, "bginit_FromNPMLE-plot1.pdf"))
+  # plot(tseq,exp(ht),type="l")
+  # lines(bbt_NPMLE[,1],exp(bbt_NPMLE[,2]),col="blue")
+  # lines(tseq,h.fun(tseq,knots,Boundary.knots,bg.init.NPMLE),col="red")
+  # dev.off()
+  
+  # pdf(file=paste0(outdir, "bginit_FromNPMLE-plot2.pdf"))
+  # plot(tseq,ht,type="l")
+  # lines(bbt_NPMLE[,1],bbt_NPMLE[,2],col="blue")
+  # lines(tseq,log(h.fun(tseq,knots,Boundary.knots,bg.init.NPMLE)),col="red")
+  # write.table(bg.init.NPMLE,paste0(datadir,"ParEst_new_std_orth_base/bginit_FromNPMLE.dat"),row.names = FALSE)
+  # dev.off()
+  
+  # ####--Option III: arbitary initial but with some ridge
+  # bg.init = rep(0.1,q)
+  # bb.init = rep(0,ncol(Z))
+  # lam     = 0
+  
+  ## optim with initial bg_bm
   bgbm.init  = c(bg.init.Cheng,bb_Cheng)
+  # bgbm.init  = c(bg.init.NPMLE,bb_NPMLE)
+  
   lam        = 0
   bgbm.optim = optim(par = bgbm.init,fn=function(x) SurvLoglik(bg=x[1:q],bb=x[-c(1:q)],knots,Boundary.knots,Delta,SX,Z)$likelihood,
                      gr=function(x) SurvLoglik(bg=x[1:q],bb=x[-c(1:q)],knots,Boundary.knots,Delta,SX,Z,likelihood = FALSE,gradient = TRUE)$gradient,
                      method = "BFGS",control = list(maxit=100000))
+  
+  ## optim with initial bg_bm
   lam.glasso = sort(seq(0,0.003,1e-6),decreasing = TRUE)
   bgbm.optim.glasso = gglasso.Approx.BSNP(bgbm.optim$par[1:q],bgbm.optim$par[-c(1:q)],knots,Boundary.knots,Delta,SX,Z,0,lam.glasso,group)
-  mo21      = which.min(bgbm.optim.glasso$AIC.LSA) # AIC
-  mo22      = which.min(bgbm.optim.glasso$BIC.LSA) # BIC
-  mo23      = which.min(bgbm.optim.glasso$AIC.Orig) # AIC on original model
-  mo24      = which.min(bgbm.optim.glasso$BIC.Orig) # BIC on original model
+  
+  # AIC
+  mo21      = which.min(bgbm.optim.glasso$AIC.LSA)
+  # BIC
+  mo22      = which.min(bgbm.optim.glasso$BIC.LSA)
+  # AIC on original model
+  mo23      = which.min(bgbm.optim.glasso$AIC.Orig)
+  # BIC on original model
+  mo24      = which.min(bgbm.optim.glasso$BIC.Orig)
   # non-zero parameters
   nzpar     = cbind(bgbm.optim.glasso$beta[,c(mo21,mo22,mo23,mo24)])!=0
+  
   nzpar     = rbind(matrix(TRUE,nrow=q+4,ncol=4),nzpar[-c(1:4),])
+  
   bgbm.optim.glasso = sapply(1:4,function(i){
     tmp = rep(0,q+ncol(Z))
     tmp[nzpar[,i]] = optim(par = bgbm.optim$par[nzpar[,i]],fn=function(x) SurvLoglik.nz(bg=x[1:q],bb=x[-c(1:q)],nzpar[-c(1:q),i],knots,Boundary.knots,Delta,SX,Z)$likelihood,
@@ -438,7 +547,12 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
   bgbm.optim.glasso = list(bgbb = bgbm.optim.glasso,
                            lam.glasso = lam.glasso[c(mo21,mo22,mo23,mo24)])
   colnames(bgbm.optim.glasso$bgbb) = names(bgbm.optim.glasso$lam.glasso) = c("AIC","BIC","AIC.Orig","BIC.Orig")
+  
   bgbbest  = cbind(bgbm.init,bgbm.optim$par,bgbm.optim.glasso$bgbb)
+  
+  # write.table(bgbbest,paste0(outdir,"bgbbest_FromChengInit_BFGS.dat"),row.names = FALSE)
+  #write.table(bgbbest,paste0(datadir,"ParEst_new_std_orth_base/bgbbest_FromNPMLEInit_BFGS.dat"),row.names = FALSE)
+  
   tree.fit = treefit(Delta,Z[,-c(1:length(TrainSurv_pred_org))]) #??????
   set.seed(seed2)
   train    = sample(1:nn,nn*0.75)
@@ -446,13 +560,20 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
   vars     = logi.fit$vars
   vars     = sapply(vars,function(x) substr(x,1,nchar(x)-3),USE.NAMES = FALSE)
   vars     = unique(vars)
+  
   wei      = GetWei(TrainPK,vars,Delta,SX)
+  
+  #####################
+  ### validation
+  #####################
   SX     = ValidSurv$sx
   SC     = ValidSurv$sc
   Delta  = ValidSurv$delta
   ## same PCA basis as training set
   Z      = as.matrix(cbind(ValidSurv[,paste0("base_pred", 1:length(ValidSurv_pred_org))],ValidFt))
+  
   Z      = {Z-VTM(meanZ,nrow(Z))}/VTM(sdZ,nrow(Z))
+  
   ValidFt_PCA = do.call(cbind,sapply(1:length(codes),function(i){
     if(length(codesK[[i]])>0){
       #tmp  = matrix(Z[,codesK[[i]]+4],ncol=length(codesK[[i]]))
@@ -466,6 +587,8 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
   }))
   colnames(ValidFt_PCA) = PCAnames
   Z        = cbind(Z[,1:length(ValidSurv_pred_org)],ValidFt_PCA)
+  # write.table(Z,paste0(datadir,"CleanData_new_std/ValidFt_PCA_Orth_Base.dat"))
+  
   endF     = quantile(SX,0.9)
   t.grid   = tseq[seq(9,length(tseq),10)]
   G_fit  = survfit(Surv(SX,1-Delta)~1)
@@ -481,6 +604,7 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
   G_SX[sort(SX,index.return=TRUE)$ix] = G_SX
   G_SX[G_SX==0] = min(G_SX[G_SX>0])
   G_tseq[G_tseq==0] = min(G_tseq[G_tseq>0])
+  
   tmp   = GetPrecAll.Comb(bgbbest,bb_Cheng,bbt_Cheng,
                           bb_NPMLE,bbt_NPMLE[-1,],SX,SX,SC,Delta,Z,tseq,t.grid,
                           knots,Boundary.knots,G_SX,G_tseq,endF,Tend,
@@ -489,12 +613,23 @@ petler <- function(object, cov_group = NULL, thresh = 0.7, PCAthresh = 0.9, seed
   colnames(tmp) = c("Init","MLE","AIC","BIC",
                     "AIC.Orig","BIC.Orig","Cheng",
                     "NPMLE","TreeTN","TreeAll","logi")
+  
   tmp2 = mean(BrierScore.KM2(tseq[tseq<=endF],SX,SX,SC,Delta,tseq,G_SX,G_tseq)[,2])
   tmp[2,] = 1-tmp[2,]/tmp2
+  
   cstats = tmp
+  # write.table(cstats,paste0(outdir,"Cstat_BrierSc_ChengInit_BFGS.dat"))
+  # write.table(tmp,paste0(outdir,"Cstat_BrierSc_NPMLEInit_BFGS.dat"))
+  
+  ################################
+  ################################
+  # Output
+  ################################
+  ################################
   output=list()
   output$bgbbest_FromChengInit_BFGS   = bgbbest
   output$Cstat_BrierSc_ChengInit_BFGS = cstats
   output$group = group
-  output
+  
+  output  
 }
--- data-raw/data_org.R
+++ data-raw/data_org.R
@@ -1,27 +1,15 @@
 library(data.table)
 
 dir <- "data-raw/data_org/"
-TrainSurv <- fread(paste0(dir, "TrainSurv.csv"))
-ValidSurv <- fread(paste0(dir, "ValidSurv.csv"))
-TrainCode <- fread(paste0(dir, "TrainCode.csv"))
-ValidCode <- fread(paste0(dir, "ValidCode.csv"))
-TrainN <- fread(paste0(dir, "TrainN.csv"))
-ValidN <- fread(paste0(dir, "ValidN.csv"))
-list(TrainSurv, ValidSurv)
-list(TrainCode, ValidCode)
-list(TrainN, ValidN)
 
-colnames(TrainSurv) <- colnames(ValidSurv) <- c(
-  "case", "delta", "sx", "sc", "base_pred1", "base_pred2", "base_pred3")
-colnames(TrainCode)[1:2] <- colnames(ValidCode)[1:2] <- c("case", "analysisfu")
-colnames(TrainN)[1] <- colnames(ValidN)[1] <- "case"
 data_org <- list(
-  TrainSurv = TrainSurv,
-  ValidSurv = ValidSurv,
-  TrainCode = TrainCode,
-  ValidCode = ValidCode,
-  TrainN = TrainN,
-  ValidN = ValidN)
-str(data_org)
+  TrainSurv = fread(paste0(dir, "TrainSurv.csv")),
+  ValidSurv = fread(paste0(dir, "ValidSurv.csv")),
+  TrainCode = fread(paste0(dir, "TrainCode.csv")),
+  ValidCode = fread(paste0(dir, "ValidCode.csv")))
+
+# colnames(data_org$TrainSurv) <- colnames(data_org$ValidSurv) <- c(
+#   "case", "delta", "sx", "sc", "base_pred1", "base_pred2", "base_pred3")
+# colnames(data_org$TrainCode)[1:2] <- colnames(data_org$ValidCode)[1:2] <- c("case", "analysisfu")
 
 usethis::use_data(data_org, overwrite = TRUE)
