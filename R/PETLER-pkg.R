f5b7cd9a12f559fd4a61a22a4a18d68b51ad8a4d
Ming Yang <hustyangming@gmail.com>
2020-03-31 11:19:30 +0000

    update

--- R/PETLER-pkg.R
+++ R/PETLER-pkg.R
@@ -122,6 +122,7 @@ petler.fpca <- function(data, PPIC_K = FALSE, n.grid = 401, propvar = 0.85, n_co
     # cat(i, "\n")
     tmp2 <- ValidN[, i + 1] > 0
     ValidNP <- ValidN[tmp2, i + 1]
+    names(ValidNP) <- rownames(ValidN[tmp2, ])
     ### PKs from Two-step procedure
     txt <- paste0("t=with(ValidCode,unlist(sapply(seq_along(month),function(j) rep(month[j],",codes[i],"[j]))))")
     eval(parse(text = txt))
@@ -136,6 +137,7 @@ petler.fpca <- function(data, PPIC_K = FALSE, n.grid = 401, propvar = 0.85, n_co
     t1 <- t1[!is.na(t1)]
     tmp <- PP.FPCA.Pred(t, ValidNP, mean.fun[[i]], basis.fun[[i]], K[i])
     FPCA[[i]]$ValidPred <- tmp
+    # rownames(FPCA[[i]]$ValidPred$scores) <- unique(ValidCode[ValidCode[] > 0, 1])
     ft.e.tmp <- cbind(matrix(ValidFU, nrow = length(ValidFU), ncol = 3), -tmp$baseline[1], log(1 + ValidN[, i + 1]))
     nns <- sum(tmp2)
     ft.e.tmp[tmp2, 1] <- t1
--- R/PPFPCA.R
+++ R/PPFPCA.R
@@ -1,3 +1,35 @@
+########################################################################
+############        Predict Scores for New Subjects         ############     
+########################################################################
+### make prediction for patients with or without codes
+PP.FPCA.Pred <- function(t, N, mean.fun, eigen.fun, K) {
+  NZ <- N == 0
+  delta <- mean.fun[2, 1] - mean.fun[1, 1]
+  baseline <- as.numeric(t(eigen.fun[, -1]) %*% mean.fun[, -1] * delta) # second term in xi
+  tmp2 <- apply(eigen.fun[, -1], 2, function(s) approx(x = mean.fun$x, y = s, xout = t)$y) ### check whether we need parallel this
+  indx <- rep(1:length(N[!NZ]), N[!NZ])
+  xi <- -baseline + t(apply(tmp2, 2, FUN = function(x) tapply(x, indx, mean))) # FPC scores, ith column for ith patient
+  if (K == 1) {
+    tmp <- mean.fun[, -1] + outer(eigen.fun[, 2], xi[1, ]) # density functions
+  } else {
+    tmp <- as.numeric(mean.fun[, -1]) + as.matrix(eigen.fun[, 1:K + 1]) %*% xi[1:K, ] # density functions
+  }
+  tmp <- apply(tmp, 2, function(x) {
+    x[x < 0] <- 0 # non-negative
+    x <- x / sum(x) # integrate to delta^2
+    return(x)
+  })
+  tmp <- tmp / delta
+  tmp2 <- {tmp[-c(1:2), ] -tmp[-c(1:2 + length(mean.fun[, 1]) - 2), ]} / diff(mean.fun[, 1], lag = 2)
+  derivatives <- cbind({mean.fun[-c(1:2), 1] + mean.fun[-c(1:2 + length(mean.fun[, 1]) - 2), 1]} / 2, tmp2)
+  scores <- t(xi)
+  rownames(scores) <- names(N)
+  list(scores = scores, 
+       densities = cbind(mean.fun[, 1], tmp), 
+       derivatives = derivatives, 
+       baseline = baseline)
+}
+
 VTM <- function(vc, dm) matrix(vc, ncol = length(vc), nrow = dm, byrow = TRUE)
 
 FPC_Kern_S <- function(x, t, N, h1, h2) {
@@ -53,36 +85,6 @@ FPC.Kern.S <- function(x, t, N, h1 = NULL, h2 = NULL, bw = "ucv", nsubs = NULL, 
   f_mu <- Reduce("+", L1) / sum(N)
   G <- Reduce("+", L2) / sum(N * (N - 1)) - tcrossprod(f_mu)
   list(f_mu = f_mu, G = G)
-}
-
-########################################################################
-############        Predict Scores for New Subjects         ############     
-########################################################################
-### make prediction for patients with or without codes
-PP.FPCA.Pred <- function(t, N, mean.fun, eigen.fun, K, parallel = FALSE) {
-  NZ <- N == 0
-  delta <- mean.fun[2, 1] - mean.fun[1, 1]
-  baseline <- as.numeric(t(eigen.fun[, -1]) %*% mean.fun[, -1] * delta) # second term in xi
-  tmp2 <- apply(eigen.fun[, -1], 2, function(s) approx(x = mean.fun$x, y = s, xout = t)$y) ### check whether we need parallel this
-  indx <- rep(1:length(N[!NZ]), N[!NZ])
-  xi <- -baseline + t(apply(tmp2, 2, FUN = function(x) tapply(x, indx, mean))) # FPC scores, ith column for ith patient
-  if (K == 1) {
-    tmp <- mean.fun[, -1] + outer(eigen.fun[, 2], xi[1, ]) # density functions
-  } else {
-    tmp <- as.numeric(mean.fun[, -1]) + as.matrix(eigen.fun[, 1:K + 1]) %*% xi[1:K, ] # density functions
-  }
-  tmp <- apply(tmp, 2, function(x) {
-    x[x < 0] <- 0 # non-negative
-    x <- x / sum(x) # integrate to delta^2
-    return(x)
-  })
-  tmp <- tmp / delta
-  tmp2 <- {tmp[-c(1:2), ] -tmp[-c(1:2 + length(mean.fun[, 1]) - 2), ]} / diff(mean.fun[, 1], lag = 2)
-  derivatives <- cbind({mean.fun[-c(1:2), 1] + mean.fun[-c(1:2 + length(mean.fun[, 1]) - 2), 1]} / 2, tmp2)
-  list(scores = t(xi), 
-       densities = cbind(mean.fun[, 1], tmp), 
-       derivatives = derivatives, 
-       baseline = baseline)
 }
 
 ######################################################################
