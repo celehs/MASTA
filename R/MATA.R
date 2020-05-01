8821ab8da5ee2ce57fd98ea7ffe818fb16705095
celehs <tcai@hsph.harvard.edu>
2020-05-01 09:37:13 +0000

    2020-05-01

--- R/MATA.R
+++ R/MATA.R
@@ -1,10 +1,10 @@
-#' @name PETLER-package
-#' @aliases PETLER
+#' @name MATA-package
+#' @aliases MATA
 #' @docType package
-#' @title PETLER Package
+#' @title MATA Package
 #' @description
 #' Implements an algorithm to identify the occurrence of event outcome from trajectories of several predictors.
-#' @details Visit the documentation website for more details. https://celehs.github.io/PETLER/ 
+#' @details Visit the documentation website for more details. https://celehs.github.io/MATA/ 
 #' @import survival doParallel foreach data.table survC1 rootSolve splines glmnet gglasso rpart rpart.utils  
 #' @importFrom graphics hist 
 #' @importFrom utils head
@@ -12,8 +12,8 @@
 #' coef cor density dnorm glm knots median optim optimize prcomp predict quantile runif sd stepfun uniroot var
 NULL
 
-#' @title Sample Data of the PETLER Pacakge
-#' @description A list of training and validation data for illustrating the use of the PETLER algorithm
+#' @title Sample Data of the MATA Pacakge
+#' @description A list of training and validation data for illustrating the use of the MATA algorithm
 #' @format A list with 4 data tables:
 #' \describe{
 #'   \item{TrainSurv}{baseline survival data for training (labeled); 1st colum: patient id, 2nd colum: event indicator (1=event, 0=censoring), 3rd colum: event time, 4th colum: follow-up time, 5th colum--: covariates.}
