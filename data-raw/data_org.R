5bcd38e61ce2a8dd7256fb55c069b55fa04aad31
Ming Yang <hustyangming@gmail.com>
2020-08-10 09:11:19 +0000

    2020-08-10

--- data-raw/data_org.R
+++ data-raw/data_org.R
@@ -1,10 +1,29 @@
 library(data.table)
 
 dir <- "data-raw/data_org/"
+
+TrainSurv <- fread(paste0(dir, "TrainSurv.csv"))
+ValidSurv <- fread(paste0(dir, "ValidSurv.csv"))
+TrainCode <- fread(paste0(dir, "TrainCode.csv"))
+ValidCode <- fread(paste0(dir, "ValidCode.csv"))
+
+TrainLong <- ValidLong <- vector("list", 3)
+TrainLong[[1]] <- subset(TrainCode[, c(1:3, 4)], pred1 != 0)
+TrainLong[[2]] <- subset(TrainCode[, c(1:3, 5)], pred2 != 0)
+TrainLong[[3]] <- subset(TrainCode[, c(1:3, 6)], pred3 != 0)
+ValidLong[[1]] <- subset(ValidCode[, c(1:3, 4)], pred1 != 0)
+ValidLong[[2]] <- subset(ValidCode[, c(1:3, 5)], pred2 != 0)
+ValidLong[[3]] <- subset(ValidCode[, c(1:3, 6)], pred3 != 0)
+
 data_org <- list(
-  TrainSurv = fread(paste0(dir, "TrainSurv.csv")),
-  ValidSurv = fread(paste0(dir, "ValidSurv.csv")),
-  TrainCode = fread(paste0(dir, "TrainCode.csv")),
-  ValidCode = fread(paste0(dir, "ValidCode.csv")))
+  TrainSurv = TrainSurv,
+  ValidSurv = ValidSurv,
+  TrainCode = TrainCode,
+  ValidCode = ValidCode)
+
+data_org2 <- list(
+  TrainLong = TrainLong,
+  ValidLong = ValidLong)
 
 usethis::use_data(data_org, overwrite = TRUE)
+usethis::use_data(data_org2, overwrite = TRUE)
