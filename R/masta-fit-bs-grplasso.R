dd1ba005fe4c7eeb4965b876a50bdf624f23c5d4
unoichi <hajime@uno-san.com>
2020-10-12 17:19:25 -0400

    Update masta-fit-bs-grplasso.R
    Deleted GetPK because it is in IFPCA

--- R/masta-fit-bs-grplasso.R
+++ R/masta-fit-bs-grplasso.R
@@ -1,16 +1,3 @@
-GetPK <- function(id, t, tseq, nn) {
-  PK <- rep(NA, nn)
-  e <- diff(tseq)
-  aa <- tapply(t, id, FUN = function(t) {
-    x <- hist(t, breaks = tseq, plot = FALSE)$counts
-    avg_sp <- cumsum(x) / tseq[-1]
-    avg_sp2 <- c(0, head(avg_sp, -1))
-    which.max((avg_sp - avg_sp2) / (avg_sp2 + e))
-  })
-  PK[unique(id)] <- tseq[aa + 1]
-  PK
-}
-
 ### All functions need in simulations
 ### Survival likelihood; Ridge, Glasso; Prediction Accuracy; Rule based algorithm (comparison)
 
